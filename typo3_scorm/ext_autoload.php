6ea890c31b000cfc030142bb97f792ab43af5fbf
Nirmalya Mondal <typo3india@gmail.com>
2016-03-28 21:46:04 +0530

    Removed SCORM Directory

--- typo3_scorm/ext_autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('typo3_scorm');
-
-    return array(   
-        'ScormEngineService' => $extensionPath.'Resources/Private/PHP/scormcloud/ScormEngineService.php',  
-    );
-
-?>
